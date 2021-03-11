85d16b6ebcc480d8a5ab484f7915d8a0201d05b9
Zdravko Gyurov <zdravko.gyurov97@gmail.com>
2021-03-11 18:38:26 +0200

    Add db test

--- db/db_test.go
+++ db/db_test.go
@@ -1,7 +1,10 @@
 package db_test
 
 import (
+	"context"
+	"grader/db"
 	"testing"
+	"time"
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
@@ -13,9 +16,23 @@ func TestBooks(t *testing.T) {
 }
 
 var _ = Describe("DB", func() {
+	var (
+		ctx    context.Context
+		cancel context.CancelFunc
+	)
+
+	BeforeSuite(func() {
+		ctx, cancel = context.WithTimeout(context.Background(), 30*time.Second)
+	})
+
+	AfterSuite(func() {
+		cancel()
+	})
+
 	Describe("Connecting to mongo DB", func() {
 		It("Should connect and ping successfully", func() {
-			Expect(1).To(Equal(1))
+			_, err := db.Connect(ctx)
+			Expect(err).ToNot(HaveOccurred())
 		})
 	})
 })
